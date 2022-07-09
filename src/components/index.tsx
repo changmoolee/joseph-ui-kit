cb6a17cab4fce213e56f73f14e1627943e0a03d4
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-07-09 23:17:05 +0900

    Deploy: npm에 publish 하기 위한 코드 변경

---
+++ src/components/index.tsx
@@ -0,0 +1,21 @@
+import Accordion from "./Accordion";
+import Button from "./Button";
+import DropDown from "./DropDown";
+import Loading from "./Loading";
+import Modal from "./Modal";
+import Search from "./Search";
+import SkeletonUI from "./SkeletonUI";
+import Tabs from "./Tabs";
+import Toggle from "./Toggle";
+
+export {
+  Accordion,
+  Button,
+  DropDown,
+  Loading,
+  Modal,
+  Search,
+  SkeletonUI,
+  Tabs,
+  Toggle,
+};
