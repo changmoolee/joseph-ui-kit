fea77dc8517f811fa4888e7f537132e9348f5f74
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-08-01 11:14:44 +0900

    Feat: Typography 패키지 내부에  font family 속성 추가 / sass 사용자를 위한 패키지 추가

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,48 +1,56 @@
 export const UtilityStyles = {
   inline: {
     code01: {
+      fontFamily: "IBM Plex Mono, monospace",
       fontSize: "12px",
       lineHeight: "16px",
       fontWeight: "400",
       letterSpacing: "0.32px",
     },
     code02: {
+      fontFamily: "IBM Plex Mono, monospace",
       fontSize: "14px",
       lineHeight: "20px",
       fontWeight: "400",
       letterSpacing: "0.32px",
     },
     label01: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "12px",
       lineHeight: "16px",
       fontWeight: "400",
       letterSpacing: "0.32px",
     },
     label02: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "14px",
       lineHeight: "18px",
       fontWeight: "400",
       letterSpacing: "0.16px",
     },
     helperText01: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "12px",
       lineHeight: "16px",
       fontWeight: "400",
       letterSpacing: "0.32px",
     },
     helperText02: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "14px",
       lineHeight: "18px",
       fontWeight: "400",
       letterSpacing: "0.16px",
     },
     legal01: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "12px",
       lineHeight: "16px",
       fontWeight: "400",
       letterSpacing: "0.32px",
     },
     legal02: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "14px",
       lineHeight: "18px",
       fontWeight: "400",
@@ -51,48 +59,56 @@ export const UtilityStyles = {
   },
   external: {
     code01: `
+      font-family: "IBM Plex Mono", monospace;
       font-size: 12px;
       line-height: 16px;
       font-weight: 400;
       letter-spacing: 0.32px;
     `,
     code02: `
+      font-family: "IBM Plex Mono", monospace;
       font-size: 14px;
       line-height: 20px;
       font-weight: 400;
       letter-spacing: 0.32px;
     `,
     label01: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 12px;
       line-height: 16px;
       font-weight: 400;
       letter-spacing: 0.32px;
     `,
     label02: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 14px;
       line-height: 18px;
       font-weight: 400;
       letter-spacing: 0.16px;
     `,
     helperText01: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 12px;
       line-height: 16px;
       font-weight: 400;
       letter-spacing: 0.32px;
     `,
     helperText02: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 14px;
       line-height: 18px;
       font-weight: 400;
       letter-spacing: 0.16px;
     `,
     legal01: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 12px;
       line-height: 16px;
       font-weight: 400;
       letter-spacing: 0.32px;
     `,
     legal02: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 14px;
       line-height: 18px;
       font-weight: 400;
@@ -104,24 +120,28 @@ export const UtilityStyles = {
 export const BodyStyles = {
   inline: {
     bodyCompact01: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "14px",
       lineHeight: "18px",
       fontWeight: "400",
       letterSpacing: "0.16px",
     },
     bodyCompact02: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "16px",
       lineHeight: "22px",
       fontWeight: "400",
       letterSpacing: "0px",
     },
     body01: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "14px",
       lineHeight: "20px",
       fontWeight: "400",
       letterSpacing: "0.16px",
     },
     body02: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "16px",
       lineHeight: "24px",
       fontWeight: "400",
@@ -130,24 +150,28 @@ export const BodyStyles = {
   },
   external: {
     bodyCompact01: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 14px;
       line-height: 18px;
       font-weight: 400;
       letter-spacing: 0.16px;
     `,
     bodyCompact02: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 16px;
       line-height: 22px;
       font-weight: 400;
       letter-spacing: 0px;
     `,
     body01: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 14px;
       line-height: 20px;
       font-weight: 400;
       letter-spacing: 0.16px;
     `,
     body02: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 16px;
       line-height: 24px;
       font-weight: 400;
@@ -159,54 +183,63 @@ export const BodyStyles = {
 export const FixedHeadingStyles = {
   inline: {
     headingCompact01: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "14px",
       lineHeight: "18px",
       fontWeight: "400",
       letterSpacing: "0.16px",
     },
     headingCompact02: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "16px",
       lineHeight: "22px",
       fontWeight: "400",
       letterSpacing: "0px",
     },
     heading01: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "14px",
       lineHeight: "20px",
       fontWeight: "600",
       letterSpacing: "0.16px",
     },
     heading02: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "16px",
       lineHeight: "24px",
       fontWeight: "600",
       letterSpacing: "0px",
     },
     heading03: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "20px",
       lineHeight: "28px",
       fontWeight: "400",
       letterSpacing: "0px",
     },
     heading04: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "28px",
       lineHeight: "36px",
       fontWeight: "400",
       letterSpacing: "0px",
     },
     heading05: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "32px",
       lineHeight: "40px",
       fontWeight: "400",
       letterSpacing: "0px",
     },
     heading06: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "42px",
       lineHeight: "50px",
       fontWeight: "300",
       letterSpacing: "0px",
     },
     heading07: {
+      fontFamily: "IBM Plex Sans, sans-serif",
       fontSize: "54px",
       lineHeight: "64px",
       fontWeight: "300",
@@ -215,54 +248,63 @@ export const FixedHeadingStyles = {
   },
   external: {
     headingCompact01: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 14px;
       lineHeight: 18px;
       font-weight: 400;
       letterSpacing: 0.16px;
     `,
     headingCompact02: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 16px;
       lineHeight: 22px;
       font-weight: 400;
       letter-spacing: 0px;
     `,
     heading01: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 14px;
       line-height: 20px;
       font-weight: 600;
       letter-spacing: 0.16px;
     `,
     heading02: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 16px;
       line-height: 24px;
       font-weight: 600;
       letter-spacing: 0px;
     `,
     heading03: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 20px;
       line-height: 28px;
       font-weight: 400;
       letter-spacing: 0px;
     `,
     heading04: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 28px;
       line-height: 36px;
       font-weight: 400;
       letter-spacing: 0px;
     `,
     heading05: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 32px;
       line-height: 40px;
       font-weight: 400;
       letter-spacing: 0px;
     `,
     heading06: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 42px;
       line-height: 50px;
       font-weight: 300;
       letter-spacing: 0px;
     `,
     heading07: `
+      font-family: "IBM Plex Sans", sans-serif;
       font-size: 54px;
       line-height: 64px;
       font-weight: 300;
