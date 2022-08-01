6c5e5f6aecb47daec5adc3a5324c3bcf77d51e0f
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-08-01 22:07:21 +0900

    Feat: Search ui inputRef, onKeyUp 속성 삭제 후 onChange, onKeyDown 속성 추가

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -6,21 +6,21 @@ import Close from "../../assets/icon/Close";
 import Magnifier from "../../assets/icon/Magnifier";
 
 type SearchProps = {
-  inputRef?: React.MutableRefObject<HTMLInputElement | null>;
   width?: string;
   disabled?: boolean;
   autoComplete?: string;
   placeholder?: string;
-  onKeyUp?: (event: any) => void;
+  onKeyDown?: (event: any) => void;
+  onChange?: (data: { value: string }) => void;
 };
 
 const Search = ({
-  inputRef,
   width = "100%",
   disabled = false,
   autoComplete = "off",
   placeholder = "placeholder",
-  onKeyUp = () => {},
+  onKeyDown = () => {},
+  onChange = () => {},
 }: SearchProps) => {
   const [value, setValue] = useState("");
 
@@ -30,6 +30,7 @@ const Search = ({
 
   const updateValue = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
+    onChange({ value: event.target.value });
   };
 
   return (
@@ -39,14 +40,13 @@ const Search = ({
       </span>
       <input
         id="search"
-        ref={inputRef}
         css={style.input}
         disabled={disabled}
         autoComplete={autoComplete}
         value={value}
         placeholder={placeholder}
         onChange={updateValue}
-        onKeyUp={onKeyUp}
+        onKeyDown={onKeyDown}
       />
       <span css={style.clearIcon(disabled)} onClick={clearInput}>
         <Close width={16} height={16} />
