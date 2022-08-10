295f18b0df490ad0bbdd45e7eeb81ecbe15e5f06
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-08-10 18:43:33 +0900

    Feat: TextInput ui 기능 추가

---
+++ src/components/TextInput/TextInput.styles.tsx
@@ -0,0 +1,53 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { UtilityStyles, BodyStyles } from "../Typography/Typography";
+
+export const container = (width: string, disabled: boolean) => css`
+  width: ${width};
+  height: 40px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  border-bottom: ${disabled ? "1px solid transparent" : "1px solid black"};
+  background-color: #f4f4f4;
+  color: #525252;
+  box-sizing: border-box;
+  opacity: ${disabled ? 0.5 : 1};
+  cursor: ${disabled ? "not-allowed" : "auto"};
+  :focus-within {
+    border-bottom: 1px solid transparent;
+    outline-width: 2px;
+    outline-style: solid;
+    outline-color: #0f62fe;
+    outline-offset: -2px;
+  }
+`;
+
+export const input = css`
+  ${BodyStyles.bodyCompact01}
+  width: 100%;
+  height: 100%;
+  padding: 0px 16px;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  box-sizing: border-box;
+  cursor: inherit;
+  ::placeholder {
+    opacity: 0.5;
+  }
+`;
+
+export const label = (disabled: boolean) => css`
+  ${UtilityStyles.label01}
+  margin: 5px 0px;
+  color: #525252;
+  opacity: ${disabled ? 0.3 : 1};
+`;
+
+export const helperText = (disabled: boolean) => css`
+  ${UtilityStyles.helperText01}
+  margin: 5px 0px;
+  color: #525252;
+  opacity: ${disabled ? 0.3 : 1};
+`;
---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,65 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import * as style from "./TextInput.styles";
+import React, { useState } from "react";
+
+type TextInputProps = {
+  width?: string;
+  type?: string;
+  maxLength?: number;
+  disabled?: boolean;
+  placeholder?: string;
+  label?: string;
+  hideLabel?: boolean;
+  warn?: string;
+  hideWarn?: boolean;
+  onChange?: (data: { value: string }) => void;
+};
+
+const TextInput = ({
+  width = "400px",
+  type = "text",
+  maxLength = 10,
+  disabled = false,
+  placeholder = "placeholder",
+  label = "label",
+  warn = "warn",
+  hideLabel = false,
+  hideWarn = false,
+  onChange = () => {},
+}: TextInputProps) => {
+  const [value, setValue] = useState("");
+
+  const updateValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+    setValue(value);
+    onChange({ value: value });
+  };
+
+  return (
+    <>
+      {hideLabel ? null : (
+        <label htmlFor="textinput">
+          <div css={style.label(disabled)}>{label}</div>
+        </label>
+      )}
+      <div css={style.container(width, disabled)}>
+        <input
+          id="textinput"
+          css={style.input}
+          type={type}
+          maxLength={maxLength}
+          disabled={disabled}
+          value={value}
+          placeholder={placeholder}
+          onChange={updateValue}
+        />
+      </div>
+      {hideWarn ? null : <div css={style.helperText(disabled)}>{warn}</div>}
+    </>
+  );
+};
+
+export default TextInput;
