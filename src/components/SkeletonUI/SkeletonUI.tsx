3b900f09aad04abcdbc2a290b4481efb7acdfa3d
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-08-30 23:57:40 +0900

    Feat: SkletonUI width 너비 기본값 설정 - 사용자 불편 방지

--- src/components/SkeletonUI/SkeletonUI.tsx
+++ src/components/SkeletonUI/SkeletonUI.tsx
@@ -1,13 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
-import { css, keyframes } from "@emotion/react";
 import * as style from "./SkeletonUI.style";
 
 interface SkeletonUIProps extends React.HTMLAttributes<any> {
   width?: string;
 }
 
-const SkeletonUI: React.FC<SkeletonUIProps> = ({ width, children }) => {
+const SkeletonUI: React.FC<SkeletonUIProps> = ({
+  width = "100%",
+  children,
+}) => {
   return <div css={style.container(width)}>{children}</div>;
 };
 
