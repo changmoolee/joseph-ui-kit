c7dc2c14bc72a129ae87bf3a6c1fbbbd5fa49376
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-09-06 10:16:35 +0900

    Storybook: docs 내 How to use -> Note 부분 스타일 가독성 좋게 수정 (ex margin-left 사용)

--- src/shared/Global.tsx
+++ src/shared/Global.tsx
@@ -5,8 +5,11 @@ import { BodyStyles } from "../components/Typography/Typography";
 const style = css`
   ${reset}
   body {
-    ${BodyStyles.body02}
+    ${BodyStyles}// mdx 문서 폰트 적용
   }
+  h1 {
+    margin-bottom: 40px;
+  } // mdx 문서 작성 시 h1 공간 확보
 `;
 
 const GlobalStyle = () => <Global styles={style} />;
