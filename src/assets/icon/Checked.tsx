4af2f204bd757f019dcd1d8562e398bc7640e54b
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-09-23 17:11:47 +0900

    Fix: Checked svg파일 크기 수정

--- src/assets/icon/Checked.tsx
+++ src/assets/icon/Checked.tsx
@@ -6,14 +6,13 @@ const Checked = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     width={16}
     height={16}
-    tabIndex={0}
+    viewBox="0 0 10 10"
     fill="none"
     {...props}
   >
     <path
-      d="M7.721 11.984a.637.637 0 0 1-.452-.188l-2.132-2.13a.64.64 0 0 1 .9-.9l1.68 1.678 4.236-4.237a.64.64 0 1 1 .9.9l-4.69 4.69a.637.637 0 0 1-.442.187Z"
+      d="M3.721 7.984a.637.637 0 0 1-.452-.188l-2.132-2.13a.64.64 0 0 1 .9-.9l1.68 1.678 4.236-4.237a.64.64 0 1 1 .9.9l-4.69 4.69a.637.637 0 0 1-.442.187Z"
       fill="#fff"
-      className="__web-inspector-hide-shortcut__"
     />
   </svg>
 );
